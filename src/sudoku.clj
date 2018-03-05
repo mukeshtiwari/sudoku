656dfd51f54280f9e9b4c36b08528eb04569de0c
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-03-05 13:56:25 +1100

    finished all functions except last one

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -134,12 +134,41 @@
        (valid-cols? board)
        (valid-blocks? board)))
 
+(valid-solution? solved-board)
 
 (defn set-value-at [board coord new-value]
-  nil)
+   (assoc-in board coord new-value))
 
+(def before-change
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(def after-change
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 4 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(= after-change (set-value-at before-change [2 1] 4))
+
+(coord-pairs (range 9))
 (defn find-empty-point [board]
-  nil)
+  (first (filter (fn [x] (not (has-value? board x))) (coord-pairs (range 9)))))
+
+(find-empty-point sudoku-board)
+(find-empty-point solved-board)
 
 (defn solve [board]
   nil)
